087d46fe68666b2ea57036bdae82d87f6f6b285a
Aakash <akjadhav2212@gmail.com>
2024-01-29 18:02:05 +0530

    added jwt , cors and json parser

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
 const indexRouter = require('./src/routes/index');
+const cors = require('cors');
 require('dotenv').config();
 
 const port = process.env.PORT | 3000;
 
+
+app.use(express.json());
+app.use(cors());
 app.use("/api/v1",indexRouter);
 
 app.listen(port,()=>{
     console.log('server running on: ',port);
-})
+});
 
